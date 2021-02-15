6cb2ebe17b8e388e4490d23f63e8d2c592074a47
Varela <>
2021-02-15 00:20:34 -0300

    modified: now it is possible to import css file pr just write it as string

--- HTML/helpers/style.ts
+++ HTML/helpers/style.ts
@@ -1,10 +1,22 @@
 import { Bissel } from "../Bissel.js";
 
-export default function (cssText: string) {
+function cssText(cssText: string) {
     const bissel = new Bissel({}, 'style');
-    bissel.attribute("href", "./aaaa.css");
     bissel.attribute("rel", "stylesheet");
     bissel.attribute("type", "text/css");
     bissel.text(cssText);
     return bissel.element;
+}
+
+function cssLink(cssPath: string) {
+    const bissel = new Bissel({}, 'link');
+    bissel.attribute("rel", "stylesheet");
+    bissel.attribute("type", "text/css");
+    bissel.attribute("href", cssPath);
+    return bissel.element;
+}
+
+export const css = {
+        text: cssText,
+        link: cssLink
 }
